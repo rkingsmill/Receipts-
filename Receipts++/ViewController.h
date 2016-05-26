3d8c4a3b364362c0d26e2dec79c38dab168555fb
RKing <rosalyn.kingsmill@gmail.com>
2016-05-26 11:38:07 -0400

    Initial Commit

---
+++ Receipts++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts++
+//
+//  Created by Rosalyn Kingsmill on 2016-05-26.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
